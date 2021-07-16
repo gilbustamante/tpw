dfa2a48018ef0f97ac5e091be9e03e0b126b0915
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-07-15 20:01:33 -0700

    Roll header func into general API call func

--- tpw/helpers.py
+++ tpw/helpers.py
@@ -1,15 +1,15 @@
 from .models import User
+import requests
 
 def find_user(user_id: int):
     """Retrieves user from database, given a user ID"""
     return User.query.filter_by(id=user_id).first()
 
-def create_request_header(user_id: int):
-    """Returns the authorization header for API requests"""
+def auth_api_call(user_id: int, url: str):
+    """Returns JSON from call to GW2's API with provided endpoint"""
     api_key = User.query.filter_by(id=user_id).first().apikey
-    config = {
-        "headers": {
-            "Authorization": f"Bearer {api_key}"
-        }
+    headers = {
+        "Authorization": f"Bearer {api_key}"
     }
-    return config
+    req = requests.get(url, headers=headers).json()
+    return req
--- tpw/main.py
+++ tpw/main.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 from dotenv import load_dotenv
-
-# TODO: Add a helper function that returns the auth 'bearer' string
+from .helpers import create_request_header, auth_api_call
+import requests
 
 # Load environment variables
 load_dotenv()
@@ -13,3 +13,10 @@ main = Blueprint("main", __name__)
 @main.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@main.route("/api", methods=["GET"])
+@login_required
+def api_get():
+    url = "https://api.guildwars2.com/v2/account"
+    res = auth_api_call(current_user.id, url)
+    return res
