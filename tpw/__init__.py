de31fc0429d146536e95d2f6ea1f64fe547109f5
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-07-20 19:05:56 -0700

    Add timeago function to templates
    Fix relaxed div issue

--- tpw/__init__.py
+++ tpw/__init__.py
@@ -7,6 +7,8 @@ from .main import main as main_blueprint
 from .auth import auth as auth_blueprint
 from .market import market as market_blueprint
 from .models import User
+from datetime import datetime, timezone
+import timeago
 import os
 
 
@@ -24,14 +26,24 @@ login_manager.login_view = "auth.login"
 login_manager.init_app(app)
 
 
+# User loader
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+# Setup DB
 @app.before_first_request
 def setup():
     db.create_all()
+
+
+# For formatting dates in templates correctly
+@app.template_filter('timeago')
+def fromnow(date):
+    date = datetime.fromisoformat(date)
+    now = datetime.now(timezone.utc)
+    return timeago.format(date, now)
 
 
 # Config
