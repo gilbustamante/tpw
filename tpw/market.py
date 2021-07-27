e786a94f5eaf7c59913b6a61d64fa546cf880d77
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-07-27 15:16:03 -0700

    Add login requirement for market routes

--- tpw/market.py
+++ tpw/market.py
@@ -11,6 +11,7 @@ load_dotenv()
 market = Blueprint("market", __name__)
 
 @market.route("/current", methods=["GET"])
+@login_required
 def listings_current():
     # All the URLs we'll need for this route
     sell_url = "https://api.guildwars2.com/v2/commerce/transactions/current/sells"
@@ -65,6 +66,7 @@ def listings_current():
                             delivery=delivery, format_gold=format_gold)
 
 @market.route("/history", methods=["GET"])
+@login_required
 def listings_history():
     sold_url = "https://api.guildwars2.com/v2/commerce/transactions/history/sells"
     bought_url = "https://api.guildwars2.com/v2/commerce/transactions/history/buys"
