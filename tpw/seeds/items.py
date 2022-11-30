d6719fbf75d8a02b08aab090cc76121edab075c4
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2022-11-29 19:56:09 -0800

    Update items seed script

--- tpw/seeds/items.py
+++ tpw/seeds/items.py
@@ -1,19 +1,37 @@
 """
-ArenaNet allows 600 API requests per minute (x-rate-limit-limit).
-As of this writing 'x-page-total' lists 303 pages (60446 total items) so
-we should be OK iterating 303 times to grab all the items in the game.
+ArenaNet allows 600 API requests per minute (x-rate-limit-limit).  The maximum
+number of items you can view per request is 200. So the number of requests we
+need to make is (x-result-total/200).
 """
 
 import os
 
 import requests
 import mysql.connector as database
+import math
+from time import sleep
 from dotenv import load_dotenv
 
 load_dotenv()
 
 username = os.getenv("DB_USER")
 password = os.getenv("DB_PASS")
+
+"""
+So every time ArenaNet updates the game and adds items, if any of those items
+need to be displayed on the page, the app throws a fit (can't display an item
+that doesn't exist yet). This means we need to "rebuild" the db every update.
+
+I got tired of doing the math myself so I wrote the following lines to just
+automatically calculate it; all you need to do is plug in whatever the current
+x-result-total is in the API response headers:
+"""
+
+# TODO: Make one request, get the current x-result-total and plug it in below
+
+total_items = 64129 # Change this to whatever the current x-result-total is
+rate_limit = 200
+total_pages = math.ceil(total_items/rate_limit)
 
 connection = database.connect(
     user=username,
@@ -36,10 +54,11 @@ def add_items(item):
         print(f"Database Error: {e}")
 
 try:
-    for i in range(0, 303):
+    for i in range(0, total_pages):
         url = f"https://api.guildwars2.com/v2/items?page={i}&page_size=200"
         res = requests.get(url).json()
         for item in res:
             add_items(item)
+        sleep(1) # So as not to make too many requests at once
 except TypeError as e:
     print(f"Error: {e} (reached end of list?)")
